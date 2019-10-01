1849b5ff9926b1ff8793fc7c79261a8b7b19da77
MadMonkeyMan3 <michael.camp@kinkaid.org>
2019-10-01 14:54:48 -0500

    Just learned  about for-each loops.

--- src/ArrayRunner.java
+++ src/ArrayRunner.java
@@ -19,5 +19,33 @@ public class ArrayRunner
             System.out.println(myclass[i]);
         }
 
+        String[] names = {"Michael", "Brian", "Duncan", "Bennett", "Martin",
+                "Jessica","Lexy","Zoe","Jimmy","Sanjit", "Andrew", "Will"};
+
+        System.out.println("There are " + names.length + "names on the list");
+
+        String[] months = {"February","October","March","September", "July",
+                "February","November","December","August","May",
+                "September", "December"};
+
+        String[] colors = {"Green","Fuscia", "Purple", "Turquoise", "Cyan", "Purple", "Pink", "Indigo", "Green", "Blue", "green", "Space"};
+
+        for(int i = 0; i<12; i++)
+        {
+            myclass[i] = new Student(names[i], months [i], colors[i]);
+        }
+
+
+        System.out.println("Print out the other students!");
+
+        for(Student s: myclass)
+        {
+            if (s.getName().equals("Michael"))
+            {
+                System.out.print("****");
+            }
+            System.out.println(s);
+        }
+
     }
 }
